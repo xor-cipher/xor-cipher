974dbad3f753f92ac4559967ff89d86f0645291d
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2022-09-13 19:37:47 +0100

    use size_t for sizes and use C division

--- xor_cipher/cipher.pyx
+++ xor_cipher/cipher.pyx
@@ -1,20 +1,24 @@
 from libc.stdint cimport *
+import cython
 
+# Removes Python's integer checks.
+@cython.cdivision(True)
 cpdef bytes xor_static(bytes content, int key):
     cdef char* c_content = content
-    cdef uint8_t length = len(content)
+    cdef size_t length = len(content)
 
     for i in range(length):
         c_content[i] ^= key
     
     return bytes(c_content)
 
+@cython.cdivision(True)
 cpdef bytes xor_cyclic(bytes content, bytes key):
     cdef char* c_content = content
     cdef char* c_key = key
-    cdef uint8_t length = len(content)
-    cdef uint8_t key_length = len(key)
-    cdef uint8_t i = 0
+    cdef size_t length = len(content)
+    cdef size_t key_length = len(key)
+    cdef size_t i = 0
 
     for i in range(length):
         c_content[i] ^= c_key[i % key_length]
